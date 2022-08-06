7ef06ab04bfdd7d3e1930edb1f1c60b113488299
Chutchapon Soyklum <chutchapon@Chutchapons-MacBook-Pro.local>
2022-08-06 13:32:06 +0700

    add drawer and bottomnavigationbar

--- class_06082022/lib/screen/home_screen.dart
+++ class_06082022/lib/screen/home_screen.dart
@@ -11,7 +11,20 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
+      appBar: AppBar(
+        title: const Text('หน้าโฮมเพจ'),
+      ),
+      drawer: const Drawer(),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'หน้าหลัก'),
+          BottomNavigationBarItem(icon: Icon(Icons.article), label: 'ข่าวสาร'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.phone_iphone), label: 'ติดต่อ'),
+          // BottomNavigationBarItem(icon: Icon(Icons.info), label: 'เกี่ยวกับ'),
+        ],
+      ),
+      body: const Center(
         child: Text('สวัสดีชาวโลก'),
       ),
     );
