97b24f4f1e194c788a75db62302e864daf48568c
Chutchapon Soyklum <chutchapon@Chutchapons-MacBook-Pro.local>
2022-08-07 11:31:50 +0700

    change HomeScreen

--- class_07082022/lib/screen/homescreen.dart
+++ class_07082022/lib/screen/homescreen.dart
@@ -10,15 +10,19 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
+    List name = ['John Doe', 'Jane Doe', 'Jame Doe'];
+    int age = 20;
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('HomeScreen'),
-        ),
-        body: Column(
-          children: [
-            // Text("สวัสดีฉันชื่อ $name"),
-            // Text("ฉันอายุ $age ปี"),
-          ],
-        ));
+      appBar: AppBar(
+        title: const Text('HomeScreen'),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text("สวัสดีฉันชื่อ : ${name[1]}"),
+          Text("ฉันอายุ : $age ปี"),
+        ],
+      ),
+    );
   }
 }
--- class_07082022/lib/screen/playscreen.dart
+++ class_07082022/lib/screen/playscreen.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
 
 class PlayScreen extends StatefulWidget {
   const PlayScreen({Key? key}) : super(key: key);
@@ -10,6 +8,28 @@ class PlayScreen extends StatefulWidget {
 }
 
 class _PlayScreenState extends State<PlayScreen> {
+  // create int number
+  int number = 0;
+  String name = 'John Doe';
+
+  void increments() {
+    setState(() {
+      number += 1;
+    });
+  }
+
+  void decrements() {
+    setState(() {
+      number -= 1;
+    });
+  }
+
+  void changename(String Newname) {
+    setState(() {
+      name = Newname;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,7 +38,27 @@ class _PlayScreenState extends State<PlayScreen> {
       ),
       body: Column(
         children: [
-          Text('PlayScreen'),
+          Text('ชื่อ : $name'),
+          Text('ค่าปัจจุบัน : $number'),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: increments,
+                child: const Text('เพิ่ม'),
+              ),
+              ElevatedButton(
+                onPressed: decrements,
+                child: const Text('ลด'),
+              ),
+              ElevatedButton(
+                onPressed: () => {
+                  changename('Jane Doe'),
+                },
+                child: const Text('เปลี่ยนชื่อ'),
+              ),
+            ],
+          ),
         ],
       ),
     );
